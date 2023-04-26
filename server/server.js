0826029c3757cca942c127d4272e82141587f51c
mrCrow <petrgrigorec32@gmail.com>
2023-04-26 16:21:57 +0300

    for render v2

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(
     cors({
       origin: [
-        'http://localhost:3000',
+        // 'http://localhost:3000',
         "https://newsserver-vrh0.onrender.com"
     ],
     
