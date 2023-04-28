585738e86a92857814303a74b503ef52d1ce9d84
mrCrow <petrgrigorec32@gmail.com>
2023-04-28 13:47:49 +0300

    server client build folder

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const authRouter = require('./authRouter');
@@ -17,15 +19,17 @@ app.use(
     }),
 );
 
+
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
+app.use(express.static("public"));
+
 app.use("/auth", authRouter);
-
-
-
 
 const start = async() => {
     try {
         await mongoose.connect("mongodb+srv://myLocalEnvironment:qeJu0Y16qXr3W1hU@cluster0.wnvgx3j.mongodb.net/news-project?retryWrites=true&w=majority")
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+        console.log(path.join(__dirname, "..", "client", "build"));
     } catch (error) {
         console.log(error);
     }
